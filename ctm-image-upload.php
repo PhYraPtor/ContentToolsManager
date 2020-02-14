89ea95d76d9b9d37ab3c3fd2be161b02d22f2d0e
Robin OGER <oger.robin@gmail.com>
2020-02-14 13:31:54 +0100

    Ajouter les photos dans la base de données

--- ctm-image-upload.php
+++ ctm-image-upload.php
@@ -5,11 +5,13 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 // (2) FILE CHECK
 // HTML file type restriction can still miss at times
-// @TODO - Add more of your own file checks if you want.
 // E.g. Restrict upload size to prevent resource hogging.
 if (isset($_FILES['image']['tmp_name']) && isset($_POST['page'])) {
     $allowed = ["bmp", "gif", "jpg", "jpeg", "png", "webp"];
     $ext = strtolower(pathinfo($_FILES["image"]["name"], PATHINFO_EXTENSION));
+    $extension = pathinfo($_FILES["image"]["name"]);
+    $extension = $extension['extension'];
+    $extension = strtolower($extension);
     if (in_array($ext, $allowed)) {
         try {
             // $source = $_FILES["image"]["tmp_name"];
@@ -18,21 +20,34 @@ if (isset($_FILES['image']['tmp_name']) && isset($_POST['page'])) {
             // move_uploaded_file($source, $destination);
 
             $dbh = new PDO('mysql:host=robinoelou.mysql.db;dbname=robinoelou', 'robinoelou', '87HsEKcf9LAHTfM');
-            $r = 'INSERT INTO images (size) VALUES (?)';
+            $r = 'INSERT INTO images (original_name) VALUES (?)';
             $stmt= $dbh->prepare($r);
-            $stmt->execute([$_POST['size']]);
+            $stmt->execute([$_FILES["image"]["name"]]);
             $imgId = $dbh->lastInsertId();
 
             $source = $_FILES["image"]["tmp_name"];
-            $destination = $_SERVER['REQUEST_URI'] . '/' . $imgId;
+            // $destination = $_SERVER['REQUEST_URI'] . '/' . $imgId;
+            $destination = 'images/' . $_POST['page'] . '/' . $imgId . '.' . $extension;
+            if (!file_exists('./images/' . $_POST['page'])) {
+                if (!mkdir('./images/' . $_POST['page'], 0777, true)) {
+                    echo json_encode([
+                        "status" => 500,
+                        "message" => "Impossible de créer le dossier"
+                    ]);
+                    die();
+                }
+            }
 
             // @todo : faire le nom du dossier
 
             move_uploaded_file($source, $destination);
+            $size = getimagesize('./'.$destination);
+            $dbSize = json_encode($size);
 
-            $r = 'UPDATE images SET (url = ?) WHERE id = ?';
+            $r = 'UPDATE images SET url = ?, size = ? WHERE id = ?';
             $stmt = $dbh->prepare($r);
-            $stmt->execute([$destination, $imgId]);
+            $stmt->execute([$destination, $dbSize, $imgId]);
+            $link = 'http://ctm.robinoger.com/';
 
             // Réponse
 
@@ -41,7 +56,8 @@ if (isset($_FILES['image']['tmp_name']) && isset($_POST['page'])) {
                 "status" => 200,
                 "message" => "File Uploaded",
                 "url" => $link . $destination,
-                "size" => getimagesize($_FILES["image"]["name"])
+                "size" => $size,
+                "id" => $imgId
             ]);
         }
         catch (PDOException $e) {
